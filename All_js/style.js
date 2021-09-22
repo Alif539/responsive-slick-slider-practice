c57f77ee9112544d830c95716723cf1ca316123a
Alif539 <ashfaqali36069@gmail.com>
2021-09-22 21:10:42 +0600

    not fully responcive

---
+++ All_js/style.js
@@ -0,0 +1,9 @@
+// banner slider starts here
+$(document).ready(function(){
+    $('.mainslider').slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        vertical:true,
+        
+    });
+  });
